47747bc776ce9b478929ef5be365c4bfd283dd62
rollways <leoleo6109@gmail.com>
2022-01-21 11:02:53 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.route('/input_customer')
+def input_customer():
+    return render_template('input_customer.html')
+
+@app.route('/result', methods=['Get','POST'])
+def result():
+    if request.method == 'POST':
+        member_code = request.values["Name"]
+    return render_template('result.html',member_code=member_code)
+
+if __name__ == '__main__':
+    app.run()
